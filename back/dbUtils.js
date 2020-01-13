cee5c3256e552cc6eef2fed9754eb4c742cfd608
Guillaume Roux <guillaumeroux123@gmail.com>
2020-01-13 23:53:43 +0100

    cleaned

--- back/dbUtils.js
+++
@@ -1,9 +0,0 @@
-import neo4j from 'neo4j-driver';
-
-const driver = neo4j.driver(process.env.DB_URL, neo4j.auth.basic(process.env.DB_USR, process.env.DB_PWD));
-
-const getSession = () => {
-	return driver.session();
-}
-
-export default getSession;
