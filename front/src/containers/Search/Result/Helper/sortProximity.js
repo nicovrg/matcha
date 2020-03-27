dbed5f2e11754fa188fd5ddb1ebcb8252cffa682
nicovrg <nicolasvergne88@gmail.com>
2020-03-27 01:35:28 +0100

    add a checkbox to determing ascending or descending sort and start changing sorting logic, not done yet

--- front/src/containers/Search/Result/Helper/sortProximity.js
+++ front/src/containers/Search/Result/Helper/sortProximity.js
@@ -1,4 +1,4 @@
-function sortProximity(userPosition, a, b) {
+function sortProximity(userPosition, ascending, descending, a, b) {
 	let latDiff_A = userPosition.latitude - a.location.lat;
 	let lngDiff_A = userPosition.longitude - a.location.lng;
 	let latDiff_B = userPosition.latitude - b.location.lat;
@@ -7,10 +7,14 @@ function sortProximity(userPosition, a, b) {
 	let scoreA = latDiff_A + lngDiff_A;
 	let scoreB = latDiff_B + lngDiff_B;
 
-	if (scoreA >= scoreB)
+	if (scoreA >= scoreB && ascending === true)
 		return (1);
 	else
 		return (-1);
+	if (scoreA >= scoreB && descending === true)
+		return (-1);
+	else
+		return (1);
 }
 
 export default sortProximity;
