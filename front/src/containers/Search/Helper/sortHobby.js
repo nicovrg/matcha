d2fea9239072b513f3bee079ed4e99e594a42363
Guillaume Roux <guillaumeroux123@gmail.com>
2020-04-02 17:05:07 +0200

    eslint rules and fixes

--- front/src/containers/Search/Helper/sortHobby.js
+++ front/src/containers/Search/Helper/sortHobby.js
@@ -1,16 +1,16 @@
 function sortHobby(userHobbies, a, b) {
 	let resultA = 0;
-		let resultB = 0;
-		if (userHobbies && userHobbies.length) {
-			userHobbies.map(userHobby => 
-				a.hobbies.map(compareHobby => (userHobby.name === compareHobby.name) && resultA++)
-			)
-			userHobbies.map(userHobby => 
-				b.hobbies.map(compareHobby => (userHobby.name === compareHobby.name) && resultB++)
-			)
+	let resultB = 0;
+	if (userHobbies && userHobbies.length) {
+		userHobbies.map(userHobby => 
+			a.hobbies.map(compareHobby => (userHobby.name === compareHobby.name) && resultA++)
+		)
+		userHobbies.map(userHobby => 
+			b.hobbies.map(compareHobby => (userHobby.name === compareHobby.name) && resultB++)
+		)
 
-		}
-		return (resultA - resultB);
+	}
+	return (resultA - resultB);
 }
 
 export default sortHobby;
